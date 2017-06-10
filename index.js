8c69847d3e8cd7aad88f01e66a20c056f9f92a67
aburkes <aburkes@users.noreply.github.com>
2017-06-09 23:19:39 -0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var Sequelize   = require("sequelize");
-var db          = require("./database/db.js");
-var interface   = require("./interface/router.js");
+let Sequelize = require("sequelize");
+let userInterface = require("./interface/router.js");
 
 db.Connection.authenticate().then(function(err){
     if(!err){console.log("Successfully connected to database");}
     else{console.log(err);}
-})
+});
 
